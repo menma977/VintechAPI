0f58c7185d65482d6b7da68c0dfebe3a2420eb2b
saikakkoi <dyta1998@outlook.com>
2020-10-20 16:31:26 +0700

    fixing sidebar

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -1,8 +1,18 @@
 <li class="nav-header">DashBoard</li>
 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <li class="nav-item">
+      <a href="{{ route('home') }}" class="nav-link {{ request()->is(['level', 'level/*']) ? 'active' : '' }}">
+        <i class="fas fa-home"></i>
+        <p>
+          Home
+          <span class="right badge badge-danger">new</span>
+        </p>
+      </a>
+    </li>
+    <!--online-->
   <li class="nav-item">
-    <a href="{{ route('level.index') }}" class="nav-link {{ request()->is(['level', 'level/*']) ? 'active' : '' }}">
-      <i class="fas fa-globe-asia"></i>
+    <a href="{{ route('online') }}" class="nav-link {{ request()->is(['online', 'online/*']) ? 'active' : '' }}">
+      <i class="fas fa-globe-americas"></i>
       <p>
         Online Users
         <span class="right badge badge-danger">new</span>
@@ -10,85 +20,25 @@
     </a>
   </li>
   <li class="nav-item">
-    <a href="{{ route('level.index') }}" class="nav-link {{ request()->is(['level', 'level/*']) ? 'active' : '' }}">
-      <i class="fa fa-users" aria-hidden="true"></i>
+    <a href="{{ route('admin.user') }}" class="nav-link {{ request()->is(['user', 'user/*']) ? 'active' : '' }}">
+      <i class="fas fa-users"></i>
       <p>
-        Total Users
+        User List
         <span class="right badge badge-danger">new</span>
-      </p>
-    </a>
-  </li>
-  <li class="nav-item">
-    <a href="{{ route('level.index') }}" class="nav-link {{ request()->is(['level', 'level/*']) ? 'active' : '' }}">
-      <i class="far fa-chart-bar"></i>
-      <p>
-        Trading Users
-        <span class="right badge badge-danger">new</span>
-      </p>
+      </p>w
     </a>
   </li>
 </ul>
 
-<li class="nav-header">User Details</li>
+<li class="nav-header">Setting</li>
 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
   <!--User Details-->
-  <li class="nav-item">
-   <a href="{{ route('level.index') }}" class="nav-link {{ request()->is(['level', 'level/*']) ? 'active' : '' }}">
-      <i class="fa fa-users" aria-hidden="true"></i>
-      <p>
-        Users
-      </p>
-    </a>
-  </li>
-  <!--/user details-->
-
-  <!--history-->
-  <li class="nav-item">
-   <a href="{{ route('level.index') }}" class="nav-link {{ request()->is(['level', 'level/*']) ? 'active' : '' }}">
-      <i class="fas fa-history" aria-hidden="true"></i>
-      <p>
-        History
-      </p>
-    </a>
-  </li>
-  <!--/history-->
-
-  <!--withdraw-->
-  <li class="nav-item">
-    <a href="{{ route('level.index') }}" class="nav-link {{ request()->is(['level', 'level/*']) ? 'active' : '' }}">
-      <i class="fa fa-credit-card" aria-hidden="true"></i>
-      <p>
-        Withdraw
-      </p>
-    </a>
-  </li>
-
-  <!--deposit-->
-  <li class="nav-item">
-    <a href="{{ route('level.index') }}" class="nav-link {{ request()->is(['level', 'level/*']) ? 'active' : '' }}">
-      <i class="far fa-money-bill-alt"></i>
-      <p>
-        Deposit
-      </p>
-    </a>
-  </li>
-
-
-  <li class="nav-item">
-    <a href="{{ route('level.index') }}" class="nav-link {{ request()->is(['level', 'level/*']) ? 'active' : '' }}">
-      <i class="nav-icon fas fa-th"></i>
-      <p>
-        Suspand
-      </p>
-    </a>
-  </li>
-</ul>
 
 <li class="nav-header">Option</li>
 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
   <li class="nav-item">
-    <a href="{{ route('level.index') }}" class="nav-link {{ request()->is(['level', 'level/*']) ? 'active' : '' }}">
-      <i class="fal fa-code-commit"></i>
+    <a href="{{ route('setting.version') }}" class="nav-link {{ request()->is(['setting/version', 'setting/version/*']) ? 'active' : '' }}">
+      <i class="fas fa-cog"></i>
       <p>
         Version
       </p>
