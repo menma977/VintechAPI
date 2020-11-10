6a5840ef42d2b4174006c68d09c587c783d73dd2
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-11-10 14:13:38 +0700

    Update StakeController.php

--- app/Http/Controllers/API/StakeController.php
+++ app/Http/Controllers/API/StakeController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 
 class StakeController extends Controller
@@ -35,58 +36,87 @@ class StakeController extends Controller
    * @return JsonResponse
    * @throws ValidationException
    */
-  public function store(Request $request)
+  public function tread(Request $request)
   {
     $this->validate($request, [
       'fund' => 'required',
       'possibility' => 'required',
-      'result' => 'required|string',
-      'status' => 'required|string',
-      'stop' => 'required',
+      'high' => 'required',
       'sessionDoge' => 'required',
+      'seeds' => 'required',
       'walletWithdraw' => 'required',
     ]);
 
-    $newStake = new HistoryStake();
-    if ($request->status == "WIN") {
-      $res = Http::asForm()->post('https://www.999doge.com/api/web.aspx', [
-        'a' => 'Withdraw',
-        's' => $request->sessionDoge,
-        'Amount' => '0',
-        'Address' => $request->walletWithdraw,
-        'Currency' => 'doge'
-      ]);
-      $newStake->user = Auth::id();
-      $newStake->fund = $request->fund;
-      $newStake->possibility = $request->possibility;
-      $newStake->result = $request->result;
-      $newStake->status = $request->status;
-      $newStake->stop = $request->stop == "true";
-      $newStake->save();
-      if (str_contains($res->body(), 'Pending') == true) {
-        $user = User::find(Auth::id());
-        $user->stake = Carbon::now();
-        $user->save();
+    $url = "https://www.999doge.com/api/web.aspx";
 
-        HistoryStake::where('user', Auth::id())->update(['stop' => true]);
+    $stake = Http::asForm()->post($url, [
+      "a" => "PlaceBet",
+      "s" => $request->sessionDoge,
+      "Low" => "0",
+      "High" => $request->high,
+      "PayIn" => $request->fund,
+      "ProtocolVersion" => "2",
+      "ClientSeed" => $request->seeds,
+      "Currency" => "doge",
+    ]);
+
+    Log::info($stake);
+
+    if ($stake->successful() && str_contains($stake->body(), 'BetId') == true) {
+      $seed = $stake["Next"];
+      $payout = $stake["PayOut"];
+      $balanceRemaining = $stake["StartingBalance"];
+      $profit = $payout - $request->fund;
+      $balanceRemaining += $profit;
+      $isWin = $profit > 0;
+
+      $newStake = new HistoryStake();
+      if ($isWin) {
+        $res = Http::asForm()->post($url, [
+          'a' => 'Withdraw',
+          's' => $request->sessionDoge,
+          'Amount' => '0',
+          'Address' => $request->walletWithdraw,
+          'Currency' => 'doge'
+        ]);
+        Log::info($res);
+        $newStake->user = Auth::id();
+        $newStake->fund = $request->fund;
+        $newStake->possibility = $request->possibility;
+        $newStake->result = $payout;
+        $newStake->status = "WIN";
+        $newStake->stop = false;
+        $newStake->save();
+        if (str_contains($res->body(), 'Pending') == true) {
+          $user = User::find(Auth::id());
+          $user->stake = Carbon::now();
+          $user->save();
+
+          HistoryStake::where('user', Auth::id())->update(['stop' => true]);
+        } else {
+          $data = ['message' => 'your connection is not as stable, find a place with a stable connection'];
+          return response()->json($data, 500);
+        }
       } else {
-        $data = ['message' => 'pesan error'];
-        return response()->json($data, 500);
+        $newStake->user = Auth::id();
+        $newStake->fund = $request->fund;
+        $newStake->possibility = $request->possibility;
+        $newStake->result = $payout;
+        $newStake->status = "LOSE";
+        $newStake->save();
       }
-    } else {
-      $newStake->user = Auth::id();
-      $newStake->fund = $request->fund;
-      $newStake->possibility = $request->possibility;
-      $newStake->result = $request->result;
-      $newStake->status = $request->status;
-      $newStake->save();
+
+      $data = [
+        'seeds' => $seed,
+        'payout' => $payout,
+        'balanceRemaining' => $balanceRemaining,
+        'isWin' => $isWin
+      ];
+
+      return response()->json($data, 200);
     }
 
-    $data = [
-      'message' => 'data is saved',
-    ];
-
-    return response()->json($data, 200);
+    return response()->json(['message' => 'your connection is not as stable, find a place with a stable connection'], 500);
   }
 
   /**
